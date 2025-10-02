4b745436feb798a9a6a7dc7271bb7951f3acf1da
vutiendat284 <63437112+vutiendat284@users.noreply.github.com>
2025-10-02 21:30:44 +0700

    Add default route / for homepage

--- app/index.js
+++ app/index.js
@@ -2,3 +2,10 @@
 const app = require('./app');
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`lab-login app listening on http://localhost:${port}`));
+app.get('/', (req, res) => {
+  res.send('Hello Sốp, server đang chạy ngon lành!');
+});
+
+app.listen(port, () => {
+  console.log(`lab-login app listening on http://localhost:${port}`);
+});
